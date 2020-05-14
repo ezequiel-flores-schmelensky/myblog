4c5cdd3b4fa365cb967da720709b1f4f22c8ba52
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-14 11:35:42 -0500

    [feat] entradas view

--- blog/applications/entrada/managers.py
+++ blog/applications/entrada/managers.py
@@ -19,3 +19,16 @@ class EntryManager(models.Manager):
         return self.filter(
             public=True,
         ).order_by('-created')[:6]
+
+    def buscar_entrada(self, kword, categoria):
+        if len(categoria) > 0:
+            return self.filter(
+                category__short_name=categoria,
+                title__icontains=kword,
+                public=True
+            ).order_by('-created')
+        else:
+            return self.filter(
+                title__icontains=kword,
+                public=True
+            ).order_by('-created')
