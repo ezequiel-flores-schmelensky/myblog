dddb82de068071e081c40b591020b6fcc9e7e2d0
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-14 17:31:28 -0500

    [feat] login and register

--- blog/applications/users/forms.py
+++ blog/applications/users/forms.py
@@ -35,6 +35,28 @@ class UserRegisterForm(forms.ModelForm):
             'genero',
             'date_birth',
         )
+        widgets = {
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder': 'Correo Electronico...',
+                }
+            ),
+            'full_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Nombres ...',
+                }
+            ),
+            'ocupation': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ocupacion ...',
+                }
+            ),
+            'date_birth': forms.DateInput(
+                attrs={
+                    'type':'date',
+                }
+            ),
+        }
     
     def clean_password2(self):
         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
