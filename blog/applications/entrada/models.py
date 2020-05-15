7b3098156538de1246e44f8d810ca4e7223a66b7
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-15 14:06:16 -0500

    [feat] implementing slug

--- blog/applications/entrada/models.py
+++ blog/applications/entrada/models.py
@@ -1,5 +1,7 @@
+from datetime import timedelta, datetime
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
 # apps terceros
 from model_utils.models import TimeStampedModel
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -74,3 +76,17 @@ class Entry(TimeStampedModel):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        now = datetime.now()
+        total_time = timedelta(
+            hours=now.hour,
+            minutes=now.minute,
+            seconds=now.second
+        )
+        seconds = int(total_time.total_seconds())
+        slug_unique = '%s %s' % (self.title, str(seconds))
+
+        self.slug = slugify(slug_unique)
+
+        super(Entry, self).save(*args, **kwargs)
