641d846dfb963dac799fa08ab1ef65f2e53e12f6
Ezequiel Flores <supereze.bxb@gmail.com>
2020-05-18 11:25:17 -0500

    [feat] sitemap and SEO

--- blog/applications/entrada/models.py
+++ blog/applications/entrada/models.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 from django.db import models
 from django.conf import settings
 from django.template.defaultfilters import slugify
+from django.urls import reverse_lazy
 # apps terceros
 from model_utils.models import TimeStampedModel
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -76,6 +77,14 @@ class Entry(TimeStampedModel):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse_lazy(
+            'entrada_app:entry-detail',
+            kwargs={
+                'slug': self.slug
+            }
+        )
 
     def save(self, *args, **kwargs):
         now = datetime.now()
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,8 +5,13 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from applications.home.sitemap import (
+    EntrySitemap,
+    Sitemap
+)
 
-urlpatterns = [
+urlpatterns_main = [
     path('admin/', admin.site.urls),
     re_path('', include('applications.users.urls')),
     re_path('', include('applications.home.urls')),
@@ -15,3 +20,23 @@ urlpatterns = [
     # urls para ckeditor
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+sitemaps = {
+    'site':Sitemap(
+        [
+            'home_app:index'
+        ]
+    ),
+    'entradas': EntrySitemap
+}
+
+urlpatterns_sitemap = [
+    path(
+        'sitemap.xml', 
+        sitemap, 
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    )
+]
+
+urlpatterns = urlpatterns_main + urlpatterns_sitemap
